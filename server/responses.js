5a925b16bfa677749f8b17f541d2c6ccad7e350e
Jeffrey Karger <whichazelnj13@aol.com>
2018-09-23 19:27:56 -0400

    handle responses and head requests

--- server/responses.js
+++ server/responses.js
@@ -27,50 +27,54 @@ const respondJSON = (request, response, status, object) => {
   response.end();
 };
 
-//function to respond without json body
+// function to respond without json body
 const respondJSONMeta = (request, response, status) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.end();
 };
 
-//return user object as JSON
+// return user object as JSON
 const getUsers = (request, response) => {
   const responseJSON = {
     users,
   };
 
-  respondJSON(request, response, 200, responseJSON);
+  if (request.method === 'HEAD') {
+    return respondJSONMeta(request, response, 200);
+  }
+
+  return respondJSON(request, response, 200, responseJSON);
 };
 
-//function to add a user from a POST body
+// function to add a user from a POST body
 const addUser = (request, response, body) => {
-  //default json message
+  // default json message
   const responseJSON = {
     message: 'Name and age are both required.',
   };
 
-  //check to make sure we have both fields
+  // check to make sure we have both fields
   if (!body.name || !body.age) {
     responseJSON.id = 'missingParams';
     return respondJSON(request, response, 400, responseJSON);
   }
 
-  //default status code
+  // default status code
   let responseCode = 201;
 
-  //if that user's name already exists in our object return an updated
+  // if that user's name already exists in our object return an updated
   if (users[body.name]) {
     responseCode = 204;
   } else {
-    //otherwise create an object with that name
+    // otherwise create an object with that name
     users[body.name] = {};
   }
 
-  //add or update fields for this user name
+  // add or update fields for this user name
   users[body.name].name = body.name;
   users[body.name].age = body.age;
 
-  //if response is created, then set our created message
+  // if response is created, then set our created message
   if (responseCode === 201) {
     responseJSON.message = 'Created Successfully';
     return respondJSON(request, response, responseCode, responseJSON);
@@ -80,11 +84,15 @@ const addUser = (request, response, body) => {
 
 
 // function to show not found error
-const notFound = (request, response, acceptedTypes) => {
+const notFound = (request, response) => {
   // error message with a description and consistent error id
   const responseJSON = {
     message: 'The page you are looking for was not found.',
   };
+
+  if (request.method === 'HEAD') {
+    return respondJSONMeta(request, response, 404);
+  }
 
   // return a 404 not found error code
   return respondJSON(request, response, 404, responseJSON);
--- server/server.js
+++ server/server.js
@@ -5,97 +5,69 @@ const query = require('querystring');
 
 const responseHandler = require('./responses.js');
 
-const urlStruct = {
-  '/': responseHandler.getIndex,
-  '/style.css': responseHandler.getCSS,
-  '/getUsers': responseHandler.getUsers,
-  '/addUser': responseHandler.addUser,
-  notFound: responseHandler.notFound,
-};
-
 // set the port
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-//handle POST requests
+// handle POST requests
 const handlePost = (request, response, parsedUrl) => {
-  //if post is to /addUser (our only POST url)
+  // check url
   if (parsedUrl.pathname === '/addUser') {
     const res = response;
 
-    //uploads come in as a byte stream that we need 
-    //to reassemble once it's all arrived
     const body = [];
 
-    //if the upload stream errors out, just throw a
-    //a bad request and send it back 
+    // if the upload stream errors out
     request.on('error', (err) => {
       console.dir(err);
       res.statusCode = 400;
       res.end();
     });
 
-    //on 'data' is for each byte of data that comes in
-    //from the upload. We will add it to our byte array.
+    // add data to byte array
     request.on('data', (chunk) => {
-      body.push(chunk); 
+      body.push(chunk);
     });
 
-    //on end of upload stream. 
+    // on end of upload stream.
     request.on('end', () => {
-      //combine our byte array (using Buffer.concat)
-      //and convert it to a string value (in this instance)
+      // combine our byte array
       const bodyString = Buffer.concat(body).toString();
-      //since we are getting x-www-form-urlencoded data
-      //the format will be the same as querystrings
-      //Parse the string into an object by field name
+
+      // Parse the string into an object by field name
       const bodyParams = query.parse(bodyString);
 
-      //pass to our addUser function
+      // pass to our addUser function
       responseHandler.addUser(request, res, bodyParams);
     });
   }
 };
 
-//handle GET requests
+// handle GET requests
 const handleGet = (request, response, parsedUrl) => {
-  //route to correct method based on url
+  // route to correct method based on url
   if (parsedUrl.pathname === '/style.css') {
     responseHandler.getCSS(request, response);
   } else if (parsedUrl.pathname === '/getUsers') {
     responseHandler.getUsers(request, response);
-  } else {
+  } else if (parsedUrl.pathname === '/') {
     responseHandler.getIndex(request, response);
   }
+    else{
+        responseHandler.notFound(request, response);
+    }
 };
 
 const onRequest = (request, response) => {
-  //parse url into individual parts
-  //returns an object of url parts by name
+  // parse url into individual parts
+  // returns an object of url parts by name
   const parsedUrl = url.parse(request.url);
 
-  //check if method was POST, otherwise assume GET 
-  //for the sake of this example
+  // check if method was POST, otherwise assume GET
+  // for the sake of this example
   if (request.method === 'POST') {
     handlePost(request, response, parsedUrl);
   } else {
     handleGet(request, response, parsedUrl);
-  }
-};
-
-// handle HTTP requests.
-const onRequest = (request, response) => {
-  // parse the url
-  const parsedUrl = url.parse(request.url);
-
-  // grab the query parameters
-  const params = query.parse(parsedUrl.query);
-  const acceptedTypes = request.headers.accept.split(',');
-
-  // check if the path name exists
-  if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response, acceptedTypes, params);
-  } else {
-    urlStruct.notFound(request, response, params);
   }
 };
 
